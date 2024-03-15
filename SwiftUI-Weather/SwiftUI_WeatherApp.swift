59645185e793ddf4001d4c979cd7a466b1b4322d
YaninaKo <yanina.kovrakh@gmail.com>
2024-03-15 14:15:53 +0100

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Yanina Kovrakh on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
